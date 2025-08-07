7f991d7db57330878166694378353b4398f506ac
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-08-07 14:45:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const nome = "Mateus"
+let idade = 25
+let cidade = "São Paulo"
+let profissao = "Desenvolvedor de Sistemas"
+let anoatual = 2025
+
+let salario = 3460.00
+let bonus = 400.00
+let totalano = salario + bonus
+
+alert(` 
+========= RELATÓRIO DO USUÁRIO =========
+Nome: ${nome}
+Idade: ${idade}
+Ano de nascimento: ${anoatual - idade}
+Cidade: ${cidade}
+Profissão: ${profissao}
+
+Salário base: U$${salario}
+Bônus: U$${bonus}
+Total mensal: U$${salario + bonus}
+Total ano: U$${totalano * 12}
+Total anual: U$${totalano * 12 * 2}
+=========================================
+`)
