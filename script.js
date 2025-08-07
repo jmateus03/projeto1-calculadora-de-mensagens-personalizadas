3e31b4422c5b8179239d124c9ce293fe5435549b
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-08-07 14:51:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+//Etapa 1
 const nome = "Mateus"
 let idade = 25
 let cidade = "São Paulo"
 let profissao = "Desenvolvedor de Sistemas"
 let anoatual = 2025
-
+//Etapa 2
 let salario = 3460.00
 let bonus = 400.00
 let totalano = salario + bonus
-
+//Etapa 3
 alert(` 
 ========= RELATÓRIO DO USUÁRIO =========
 Nome: ${nome}
@@ -24,3 +25,4 @@ Total anual: U$${totalano * 12 * 2}
 =========================================
 
 `)
+
