9da91e7ef0cb04bbb98cc2aa3b4f503ff7bea08b
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-08-08 14:19:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,29 @@ let idade = 25
 let cidade = "São Paulo"
 let profissao = "Desenvolvedor de Sistemas"
 let anoatual = 2025
+let anoNascimento = anoatual - idade
+
 //Etapa 2
 let salario = 3460.00
 let bonus = 400.00
-let totalano = salario + bonus
+let mensal = salario + bonus
+let totalano = (salario + bonus) * 12
+let totalanual = totalano * 2
 //Etapa 3
+
 alert(` 
 ========= RELATÓRIO DO USUÁRIO =========
 Nome: ${nome}
 Idade: ${idade}
-Ano de nascimento: ${anoatual - idade}
+Ano de nascimento: ${anoNascimento}
 Cidade: ${cidade}
 Profissão: ${profissao}
 
 Salário base: U$${salario}
 Bônus: U$${bonus}
-Total mensal: U$${totalano}
-Total ano: U$${totalano * 12}
-Total anual: U$${totalano * 12 * 2}
+Total mensal: U$${mensal}
+Total ano: U$${totalano}
+Total anual: U$${totalanual}
 =========================================
 
 `)
-
